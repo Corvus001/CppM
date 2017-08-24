04a630c20756f4289515d2a2ccc883e43313d8c1
Corvus001 <ichneutae@gmail.com>
2017-08-24 15:53:02 -0300

    New Insert Function

---
+++ 9/9_1_remake/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "linked_list.h"
+
+int main() {
+
+	Linked_list l;
+	l.insert(1,0);
+	l.print();
+	l.insert(2,1);
+	l.print();
+	l.insert(3,2);
+	l.print();
+	l.insert(5,3);
+	l.print();
+	l.insert(4,3);
+	l.print();
+
+}
