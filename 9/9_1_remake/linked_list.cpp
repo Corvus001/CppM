9b67811935fd62c81f075436ad7714d41af95052
Corvus001 <ichneutae@gmail.com>
2017-08-24 15:59:25 -0300

    .

--- 9/9_1_remake/linked_list.cpp
+++ 9/9_1_remake/linked_list.cpp
@@ -21,7 +21,7 @@ Linked_list::~Linked_list() {
 
 	Linked_list_node* iter1 = _head;
 	while (iter1 != NULL) {
-		Linked_list_node* tmp = iter1.get_next();
+		Linked_list_node* tmp = iter1->get_next();
 		delete iter1;
 		iter1 = tmp;
 	}
@@ -31,12 +31,12 @@ Linked_list::~Linked_list() {
 //---------------------METHODS--------------------------------
 //------------------------------------------------------------
 
-Linked_list::print() {						// PRINT: Print Linked_List val's, starting from head
+void Linked_list::print() {						// PRINT: Print Linked_List val's, starting from head
 
 	Linked_list_node* iter = _head;
 	while (iter != NULL) {
 		cout << iter->get_value() << ' ';
-		iter = iter.get_next();
+		iter = iter->get_next();
 	}
 	cout << endl;
 	return;
@@ -48,27 +48,52 @@ bool Linked_list::insert(int val, int pos) {
 
 	if (_size < pos) return false;
 
+	// Insert at Beginning
 	if (pos == 0) {
-		Linked_list_node* new_node = new Linked_list_node(val, _head);
+
+		Linked_list_node* new_node;
+		new_node = new Linked_list_node(val, NULL);
 		_head = new_node;
-		_size++;
+		_size++;	
 
 		return true;
-	}	
-
-	Linked_list_node* iterator = _head;
-	int i = 0;
-	while (i<_size) {
-		if (i == pos) {
-			Linked_list_node* new_node = new Linked_list_node(val, iterator->get_next());
-			iterator->link(new_node);
-			_size++;
-			return true;
-		}
-		iterator = iterator->get_next();
-		i++;
 	}
-	return false;
+
+	Linked_list_node* _prev_node = _head;
+
+	while (--pos) _prev_node = _prev_node->get_next();
+
+	Linked_list_node* new_node = new Linked_list_node(val, _prev_node->get_next() );
+	_prev_node->link(new_node);
+	_size++;
+
+	return true;
+
+
+
 }
 
 // REMOVE------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
